f49f63be03e280f7873e3f79881dfb37f9ec6c75
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-21 22:59:03 +0300

    change theme & add text to splash screen

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,7 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,12 +1,22 @@
+import 'package:bookly/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(
+          Assets.imagesLogo,
+          width: 300,
+        ),
+        const SizedBox(height: 10),
+        const Text("Read Free Books"),
+      ],
     );
   }
 }
