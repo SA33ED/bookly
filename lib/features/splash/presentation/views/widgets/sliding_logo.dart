f8e5fa03def9e146f1925f664162543cf637bd41
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-23 09:46:18 +0300

    Add animation to splash view

---
+++ lib/features/splash/presentation/views/widgets/sliding_logo.dart
@@ -0,0 +1,28 @@
+
+import 'package:bookly/core/utils/app_assets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SlidingLogo extends StatelessWidget {
+  const SlidingLogo({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: SvgPicture.asset(
+              Assets.imagesLogo,
+              width: 300,
+            ),
+          );
+        });
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,21 +1,46 @@
-import 'package:bookly/core/utils/app_assets.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/sliding_logo.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    super.initState();
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        SvgPicture.asset(
-          Assets.imagesLogo,
-          width: 300,
-        ),
+        SlidingLogo(slidingAnimation: slidingAnimation),
         const SizedBox(height: 10),
-        const Text("Read Free Books"),
+        SlidingText(
+          slidingAnimation: slidingAnimation,
+          text: "Read Free Books",
+        ),
       ],
     );
   }
