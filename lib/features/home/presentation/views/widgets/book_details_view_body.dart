015eb042deea13a0a5d12463b285f8e7570575e7
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-25 16:32:06 +0300

    add title & subTitle to details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_rating_widget.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -10,16 +12,36 @@ class BookDetailsViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const SizedBox(height: 30),
           const CustomBookDetailsAppBar(),
           const SizedBox(height: 33.2),
           Padding(
             padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.17),
+                horizontal: MediaQuery.of(context).size.width * 0.2),
             child: const CustomBookImage(),
           ),
-          const Text("data"),
+          const SizedBox(height: 40),
+          const Text(
+            "The Jungle Book",
+            style: Styles.textstyle30,
+          ),
+          const SizedBox(height: 6),
+          Text(
+            "Rudyard Kilpling",
+            style: Styles.textstyle18.copyWith(
+                color: Colors.white70,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 14),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BookRatingWidget(),
+            ],
+          ),
         ],
       ),
     );
