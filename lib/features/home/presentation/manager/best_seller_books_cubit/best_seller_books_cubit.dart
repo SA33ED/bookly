24d79976c4a24dd65670d43c8e7e944c74725a86
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-28 18:23:13 +0300

    finish best seller cubit

--- lib/features/home/presentation/manager/best_seller_books_cubit/best_seller_books_cubit.dart
+++ lib/features/home/presentation/manager/best_seller_books_cubit/best_seller_books_cubit.dart
@@ -1,6 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:bookly/features/home/presentation/manager/best_seller_books_cubit/best_seller_books_state.dart';
 
 class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
-  BestSellerBooksCubit() : super(BestSellerBooksInitial());
+  BestSellerBooksCubit(this.homeRepo) : super(BestSellerBooksInitial());
+  HomeRepo homeRepo;
+
+  Future<void> fetchFeatureBooks() async {
+    emit(BestSellerBooksLoading());
+    var result = await homeRepo.fetchBestSellerBooks();
+    result.fold(
+      (failuer) {
+        emit(BestSellerBooksFailure(failuer.errMessage));
+      },
+      (books) {
+        emit(BestSellerBooksSuccess(books));
+      },
+    );
+  }
 }
