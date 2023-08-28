a3c5c1cf974962cb9eb7531ba4d01b8dd368129a
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-28 17:17:58 +0300

    create server failure from dio error

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,52 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure("Connection timeout");
+      case DioExceptionType.sendTimeout:
+        return ServerFailure("Send timeout");
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure("Receive timeout");
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure("Bad Certificate");
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioException.response!.statusCode!, dioException.response!.data!);
+
+      case DioExceptionType.cancel:
+        return ServerFailure("Cancel");
+
+      case DioExceptionType.connectionError:
+        return ServerFailure("Connection Error");
+
+      case DioExceptionType.unknown:
+        return ServerFailure("Unknown");
+      default:
+        return ServerFailure("Opps There was an error");
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 401) {
+      return ServerFailure(response['error']['mmessage']);
+    } else if (statusCode == 404) {
+      return ServerFailure("Your request not found, please try again later!");
+    } else if (statusCode == 500) {
+      return ServerFailure("Internal Server error, please try again later!");
+    } else {
+      return ServerFailure("Opps There was an error");
+    }
+  }
+}
