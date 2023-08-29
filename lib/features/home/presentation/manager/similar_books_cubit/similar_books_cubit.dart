b7d9cf22f9b380eab181e8aeee26956d6a0557d1
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-29 20:35:22 +0300

    create states & cubit methods

--- lib/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart
+++ lib/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart
@@ -1,7 +1,22 @@
 
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:bookly/features/home/presentation/manager/similar_books_cubit/similar_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksCubit extends Cubit<SimilarBooksState> {
-  SimilarBooksCubit() : super(SimilarBooksInitial());
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+  HomeRepo homeRepo;
+
+  Future<void> fetchFeatureBooks() async {
+    emit(SimilarBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failuer) {
+        emit(SimilarBooksFailure(failuer.errMessage));
+      },
+      (books) {
+        emit(SimilarBooksSuccess(books));
+      },
+    );
+  }
 }
--- lib/features/home/presentation/manager/similar_books_cubit/similar_books_state.dart
+++ lib/features/home/presentation/manager/similar_books_cubit/similar_books_state.dart
@@ -1,5 +1,21 @@
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+
 class SimilarBooksState {
   const SimilarBooksState();
 }
 
 final class SimilarBooksInitial extends SimilarBooksState {}
+
+final class SimilarBooksLoading extends SimilarBooksState {}
+
+final class SimilarBooksFailure extends SimilarBooksState {
+  final String errMessage;
+
+  const SimilarBooksFailure(this.errMessage);
+}
+
+final class SimilarBooksSuccess extends SimilarBooksState {
+  final List<BookModel> books;
+
+  const SimilarBooksSuccess(this.books);
+}
